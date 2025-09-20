59083603518dd4240b429f9b1edbd08c948a3425
Muhammad Rafiq <sameerbalti704@gmail.com>
2025-09-20 17:30:22 +0500

    again fs error fix

--- backend/src/utils/multerConfig.js
+++ backend/src/utils/multerConfig.js
@@ -1,8 +1,9 @@
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import fs from 'fs';
-import fs from "fs/promises"; // use promises API
+import fsSync from "fs";
+import fs from "fs/promises";
+// import fs from "fs"; 
 import sharp from "sharp";
 
 
@@ -10,13 +11,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Ensure directory exists
-const ensureDirectoryExists = (dir) => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
+// const ensureDirectoryExists = (dir) => {
+//   if (!fs.existsSync(dir)) {
+//     fs.mkdirSync(dir, { recursive: true });
+//     console.log(`Created directory: ${dir}`);
+//   }
+// };
+const ensureDirectoryExists = async (dir) => {
+  if (!fsSync.existsSync(dir)) {
+    await fs.mkdir(dir, { recursive: true });
     console.log(`Created directory: ${dir}`);
   }
 };
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     let uploadPath = path.join(__dirname, '../uploads/others'); // Default path
